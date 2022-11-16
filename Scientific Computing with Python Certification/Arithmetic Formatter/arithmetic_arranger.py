efc50d2ff1e3499f7f1ef132d3a4544101c15c0d
Michael Tejada <76642717+qjmttejada@users.noreply.github.com>
2022-11-16 14:51:49 +0800

    new arithmetic formatter

--- Scientific Computing with Python Certification/Arithmetic Formatter/arithmetic_arranger.py
+++ Scientific Computing with Python Certification/Arithmetic Formatter/arithmetic_arranger.py
@@ -1,64 +1,47 @@
-def arithmetic_arranger(problems, answer=False):
-    # Check the number of problems
+def arithmetic_arranger(problems, solve=False):
+
+    # Number of problems is limited to 5
     if len(problems) > 5:
         return "Error: Too many problems."
-    
-    first_operand = []
-    second_operand = []
-    operator = []
 
+    # Split problems into individual problems
+    line1 = line2 = line3 = line4 = ""
     for problem in problems:
-        pieces = problem.split()
-        first_operand.append(pieces[0])
-        operator.append(pieces[1])
-        second_operand.append(pieces[2])
+        try:
+            [operand1, operator, operand2] = problem.split()
+        except ValueError:
+            return "Error: Malformed problem"
 
-    # Check for * or /
-    if "*" in operator or "/" in operator:
-        return "Error: Operator must be '+' or '-'."
+        # Check operators
+        if operator not in "+-":
+            return "Error: Operator must be '+' or '-'."
 
-    # Check the digits
-    for i in range(len(first_operand)):
-        if not (first_operand[i].isdigit() and second_operand[i].isdigit()):
+        # Check that input is comprised of digits
+        if not operand1.isdigit() or not operand2.isdigit():
             return "Error: Numbers must only contain digits."
 
-    # Check the length
-    for i in range(len(first_operand)):
-        if len(first_operand[i]) > 4 or len(second_operand[i]) > 4:
+        # Check that numbers are 4 digits or less
+        if len(operand1) > 4 or len(operand2) > 4:
             return "Error: Numbers cannot be more than four digits."
 
-    first_line = []
-    second_line = []
-    third_line = []
-    fourth_line = []
-
-    for i in range(len(first_operand)):
-        if len(first_operand[i]) > len(second_operand[i]):
-            first_line.append(" "*2 + first_operand[i])
+        # Calculate the answer
+        if operator in "+":
+            answer = str(int(operand1) + int(operand2))
+        elif operator in "-":
+            answer = str(int(operand1) - int(operand2))
         else:
-            first_line.append(" "*(len(second_operand[i]) - len(first_operand[i]) + 2) + first_operand[i])
+            return "Error: unknown operator (this is a bug)"
 
-    for i in range(len(second_operand)):
-        if len(second_operand[i]) > len(first_operand[i]):
-            second_line.append(operator[i] + " " + second_operand[i])
-        else:
-            second_line.append(operator[i] + " "*(len(first_operand[i]) - len(second_operand[i]) + 1) + second_operand[i])
+        # Create the lines for the formatting
+        width = max(len(operand1), len(operand2))
+        line1 += "  " + " " * (width - len(operand1)) + operand1 + "    "
+        line2 += operator + " " + " " * (width - len(operand2)) + operand2 + "    "
+        line3 += "-" * (width + 2) + "    "
+        line4 += " " * ((width + 2) - len(answer)) + answer + "    "
 
-    for i in range(len(first_operand)):
-        third_line.append("-"*(max(len(first_operand[i]), len(second_operand[i])) + 2))
+    arranged_problems = line1.rstrip() + "\n" + line2.rstrip() + "\n" + line3.rstrip()
 
-    if answer:
-        for i in range(len(first_operand)):
-            if operator[i] == "+":
-                ans = str(int(first_operand[i]) + int(second_operand[i]))
-            else:
-                ans = str(int(first_operand[i]) - int(second_operand[i]))
+    if solve:
+        arranged_problems += '\n' + line4.rstrip()
 
-            if len(ans) > max(len(first_operand[i]), len(second_operand[i])):
-                fourth_line.append(" " + ans)
-            else:
-                fourth_line.append(" "*(max(len(first_operand[i]), len(second_operand[i])) - len(ans) + 2) + ans)
-        arranged_problems = "    ".join(first_line) + "\n" + "    ".join(second_line) + "\n" + "    ".join(third_line) + "\n" + "    ".join(fourth_line)
-    else:
-        arranged_problems = "    ".join(first_line) + "\n" + "    ".join(second_line) + "\n" + "    ".join(third_line)
-    return 
+    return arranged_problems
